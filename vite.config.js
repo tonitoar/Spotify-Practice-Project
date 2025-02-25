7e32592f2341531efef695e5ffe25893efceb1a7
Toni Torredemer <toni.torredemer.argiles@gmail.com>
2025-02-25 23:30:48 +0100

    vite config change for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Spotify-Practice-Project/', // Replace with your GitHub repository name
+  build: {
+    outDir: 'dist', // Ensure output is in 'dist'
+  },
+});
